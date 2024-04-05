819cfc14363e58a84e8ff190a75a322f6a1505f8
erhariprasath <54638424+erhariprasath@users.noreply.github.com>
2024-04-05 11:17:30 +0530

    new line added to the code

--- src/test/java/Package5/Ex2.java
+++ src/test/java/Package5/Ex2.java
@@ -4,7 +4,8 @@ public class Ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("HEllo");	
+		System.out.print("HEllo");
+		System.out.print("HEllo");
 		}
 
 }
